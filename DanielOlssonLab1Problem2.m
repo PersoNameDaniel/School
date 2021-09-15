2546c5c4673e5f7b71205eb17543795c4ebeaf5e
PersoNameDaniel <dmolsson@outlook.com>
2021-09-15 10:01:01 -0500

    Uploading Lab 1

---
+++ DanielOlssonLab1Problem2.m
@@ -0,0 +1,37 @@
+%{
+Class : ENGR15100: Software Tools for Engineers
+Instructor : Xiaoli Yang
+Author : [Daniel Olsson]
+Assignment : Lab [1]
+File Name : DanielOlssonLab1Problem2.m
+(eg: Xiaoli_Yang_LAB1_Problem1.m)
+Date : [08]/[30]/[2021]
+%}
+%clear workspace
+clear
+%clear screen
+clc
+%display your name
+disp('Daniel Olsson');
+disp('Starting L1 P2');
+%Start your source code here%
+
+%assigned questions%
+
+% Problem 1 %
+nthroot(3,64+abs(17))
+
+% Problem 2 %
+exp(sqrt(60+3+sqrt(49)))
+
+% Problem 3 %
+2 + floor(20/9)/2+ceil(6/9+3)/2
+
+% Problem 4 %
+(sin(0.2*(pi)))/(cos((pi)/6))+tand(60)
+
+% Problem 5 %
+(23+nthroot(3,45)/(16*0.7))+log10(589006)
+
+%End your code
+disp('Completed');
