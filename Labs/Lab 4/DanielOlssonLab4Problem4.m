9ba8d03c5870007309a514a5ada7755a816e13b3
PersoNameDaniel <dmolsson@outlook.com>
2021-10-04 21:24:15 -0500

    Problem 4 Done

--- Labs/Lab 4/DanielOlssonLab4Problem4.m
+++ Labs/Lab 4/DanielOlssonLab4Problem4.m
@@ -18,7 +18,33 @@ disp('Starting L4 P4');
 
 %assigned questions
 
+% 1)
+
+%Receiving inputs from user
+input1 = input('Enter first number: ')
+pause(5)
+
+input2 = input('Enter second number: ')
+pause(5)
+
+input3 = input('Enter third number: ')
+pause(5)
+
+% Converting inputs to array
+
+array = [input1, input2, input3]
+
+% Processing inputs from user
+operation(array)
 
 
 %End your code
 disp('Completed');
+
+% Functions
+
+% Calc Sum and Max Operations (p.s. yes, I see this function was kinda pointless, but I already made it after I realised that... the code still does all required)
+function [sumx, maxx] = operation(x)
+    sumx = sum(x)
+    maxx = max(x)
+end
