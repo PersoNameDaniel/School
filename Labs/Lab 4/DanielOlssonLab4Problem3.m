3c1ec1b8082c0a2f614cd16b9ac74f9e69c7944a
PersoNameDaniel <dmolsson@outlook.com>
2021-10-04 21:11:52 -0500

    Problem 3 Done

--- Labs/Lab 4/DanielOlssonLab4Problem3.m
+++ Labs/Lab 4/DanielOlssonLab4Problem3.m
@@ -18,7 +18,18 @@ disp('Starting L4 P3');
 
 %assigned questions
 
+% 1)
 
+% Declaring Variables
+t1 = string("23.00")
+t2 = string("45.00")
+t3 = string("48.00")
+
+v1 = string("10.05")
+v2 = string("24.23")
+v3 = string("30.50")
+
+fprintf('\t Time (s)   Velocity (m/s) \n ---------------------------------- \n\t %s \t\t %s \n\t %s \t\t %s \n\t %s \t\t %s \n', t1, v1, t2, v2, t3, v3)
 
 %End your code
 disp('Completed');
