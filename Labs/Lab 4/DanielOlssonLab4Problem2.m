6ff190717a1e0fd2acc48af046e6fe7cdba5b619
PersoNameDaniel <dmolsson@outlook.com>
2021-10-04 20:59:41 -0500

    Fixed Problem 2 format

--- Labs/Lab 4/DanielOlssonLab4Problem2.m
+++ Labs/Lab 4/DanielOlssonLab4Problem2.m
@@ -18,7 +18,8 @@ disp('Starting L4 P2');
 
 %assigned questions
 
-fprintf('Hello again from ENGR 15100!\n "----------------------------------"\n Disdplay\n a\n string\n across\n multiple lines\n "-----------------------------------"\n This is September 23, 2020.')
+% 1)
+fprintf('Hello again from ENGR 15100! \n "----------------------------------" \n Disdplay \n a \n string \n across \n multiple lines \n "-----------------------------------" \n This is September 23, 2020. \n ')
 
 %End your code
 disp('Completed');
