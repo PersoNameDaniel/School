c7cf59cd10c6ff4ef461d717aebcd77545b14197
PersoNameDaniel <dmolsson@outlook.com>
2021-10-05 11:58:12 -0500

    Created Lab 5 Files

---
+++ Labs/Lab 5/DanielOlssonLab5Problem6.m
@@ -0,0 +1,24 @@
+%{
+Class : ENGR15100: Software Tools for Engineers
+Instructor : Xiaoli Yang
+Author : [Daniel Olsson]
+Assignment : Lab [5]
+File Name : DanielOlssonLab5Problem6.m
+(eg: Xiaoli_Yang_LAB1_Problem1.m)
+Date : [10]/[5]/[2021]
+%}
+%clear workspace
+clear
+%clear screen
+clc
+%display your name
+disp('Daniel Olsson');
+disp('Starting L5 P6');
+%Start your source code here
+
+%assigned questions
+
+
+
+%End your code
+disp('Completed');
