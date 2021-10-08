9cf1e07cea45f3681174da95f112d93f63980311
PersoNameDaniel <dmolsson@outlook.com>
2021-10-08 18:22:49 -0500

    Problem 6 Done

--- Labs/Lab 5/DanielOlssonLab5Problem6.m
+++ Labs/Lab 5/DanielOlssonLab5Problem6.m
@@ -18,7 +18,26 @@ disp('Starting L5 P6');
 
 %assigned questions
 
+% 1)
 
+% Plot 1
+t1 = [1800:1:2200]
+p1 = (11.55) ./ (1 + 18.7 .* exp(-0.0193 .* (t1 - 1850)))
+
+hold on
+plot(t1, p1)
+
+xlabel('Date, years')
+ylabel('Population, Billions')
+title('World Population')
+
+% Plot 2
+t2 = [1850, 1910, 1950, 1980, 2000, 2010]
+p2 = [1.3, 1.75, 3, 4.4, 6, 6.8]
+
+plot(t2, p2, "*g")
+
+legend('Model', 'Census Data')
 
 %End your code
 disp('Completed');
