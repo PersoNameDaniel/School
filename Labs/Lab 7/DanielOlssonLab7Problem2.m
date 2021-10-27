5b85ad8ff05758a8ceae0effc63196689b02af80
PersoNameDaniel <dmolsson@outlook.com>
2021-10-27 16:37:33 -0500

    created lab 7-8 files

---
+++ Labs/Lab 7/DanielOlssonLab7Problem2.m
@@ -0,0 +1,24 @@
+%{
+Class : ENGR15100: Software Tools for Engineers
+Instructor : Xiaoli Yang
+Author : [Daniel Olsson]
+Assignment : Lab [7]
+File Name : DanielOlssonLab7Problem2.m
+(eg: Xiaoli_Yang_LAB1_Problem1.m)
+Date : [10]/[27]/[2021]
+%}
+% clear workspace
+clear
+% clear screen
+clc
+% display your name
+disp('Daniel Olsson');
+disp('Starting L7 P1');
+% Start your source code here
+
+% assigned questions
+
+
+
+% End your code
+disp('Completed');
---
+++ Labs/Lab 8/DanielOlssonLab8Problem3.m
@@ -0,0 +1,24 @@
+%{
+Class : ENGR15100: Software Tools for Engineers
+Instructor : Xiaoli Yang
+Author : [Daniel Olsson]
+Assignment : Lab [8]
+File Name : DanielOlssonLab8Problem3.m
+(eg: Xiaoli_Yang_LAB1_Problem1.m)
+Date : [11]/[1]/[2021]
+%}
+% clear workspace
+clear
+% clear screen
+clc
+% display your name
+disp('Daniel Olsson');
+disp('Starting L8 P3');
+% Start your source code here
+
+% assigned questions
+
+
+
+% End your code
+disp('Completed');
