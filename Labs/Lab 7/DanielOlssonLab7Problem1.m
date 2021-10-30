b14b0c317bd145135e7a5d69122e49c7b55b4570
PersoNameDaniel <dmolsson@outlook.com>
2021-10-30 16:29:39 -0500

    Progress on lab 7 Problem 1

--- Labs/Lab 7/DanielOlssonLab7Problem1.m
+++ Labs/Lab 7/DanielOlssonLab7Problem1.m
@@ -17,8 +17,17 @@ disp('Starting L7 P1');
 % Start your source code here
 
 % assigned questions
+a = input('scalar value of a: ')
+b = input('scalar value of b: ')
+c = input('scalar value of c:')
 
+D = ((b^2) - (4 * a * c))
 
+r1 =((-b + sqrt(D))/2 * a)
+r2 =((-b - sqrt(D))/2 * a)
+
+er1 = ((-b + sqrt((b^2) - (4 * a * c)))/(2 * a))
+er2 = ((-b - sqrt((b^2) - (4 * a * c)))/(2 * a))
 
 % End your code
 disp('Completed');
