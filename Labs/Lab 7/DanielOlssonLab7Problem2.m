ecb330b74cc7157a8d3d77b1616fba5633efc437
PersoNameDaniel <dmolsson@outlook.com>
2021-11-06 14:33:15 -0500

    Problem 2 Done

--- Labs/Lab 7/DanielOlssonLab7Problem2.m
+++ Labs/Lab 7/DanielOlssonLab7Problem2.m
@@ -18,7 +18,48 @@ disp('Starting L7 P1');
 
 % assigned questions
 
+% asking for inputs
+x = input('input x coordinate: ');
+y = input('input y coordinate: ');
 
+% testing for location
+if (x == 0) & (y == 0);
+	message = string('on the origin');
+elseif (x == 0);
+	message = string('on the x-axis');
+elseif (y == 0);
+	message = string('on the y-axis');
+elseif (x > 0) & (y > 0);
+	message = string('in quadrant I');
+elseif (x > 0) & (y < 0);
+	message = string('in quadrant II');
+elseif (x < 0) & (y > 0);
+	message = string('in quadrant III');
+elseif (x < 0) & (y < 0);
+	message = string('in quadrant IV');
+else;
+	disp('error');
+end;
+
+% finding distance from origin
+distance = round(sqrt((x^2) + (y^2)), 4);
+
+% testing if distance equals one
+if (distance == 1);
+	one = true;
+else;
+	one = false;
+end;
+
+distance = string(distance);
+
+% printing message
+fprintf('Point (%d, %d) is located %s \n', x, y, message);
+if (one == true);
+	fprintf('Point (%d, %d) is %s unit away from the origin. \n', x, y, distance);
+else;
+	fprintf('Point (%d, %d) is %s units away from the origin. \n', x, y, distance);
+end;
 
 % End your code
 disp('Completed');
