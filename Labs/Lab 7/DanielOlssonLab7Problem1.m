42c81ccda180bd41406137c0c322a81638e868bd
PersoNameDaniel <dmolsson@outlook.com>
2021-11-06 14:02:07 -0500

    Problem 1 Done

--- Labs/Lab 7/DanielOlssonLab7Problem1.m
+++ Labs/Lab 7/DanielOlssonLab7Problem1.m
@@ -17,17 +17,30 @@ disp('Starting L7 P1');
 % Start your source code here
 
 % assigned questions
-a = input('scalar value of a: ')
-b = input('scalar value of b: ')
-c = input('scalar value of c:')
 
-D = ((b^2) - (4 * a * c))
+% Quadratic Formula
+a = input('scalar value of a: ');
+b = input('scalar value of b: ');
+c = input('scalar value of c:');
 
-r1 =((-b + sqrt(D))/2 * a)
-r2 =((-b - sqrt(D))/2 * a)
+D = ((b^2) - (4 * a * c));
 
-er1 = ((-b + sqrt((b^2) - (4 * a * c)))/(2 * a))
-er2 = ((-b - sqrt((b^2) - (4 * a * c)))/(2 * a))
+%r1 =((-b + sqrt(D))/2 * a)
+%r2 =((-b - sqrt(D))/2 * a)
+
+er1 = ((-b + sqrt((b^2) - (4 * a * c)))/(2 * a));
+er2 = ((-b - sqrt((b^2) - (4 * a * c)))/(2 * a));
+
+% Processing
+if D >= 0;
+	disp ('there are two real roots')
+	disp(round(er1, 3));
+	disp(round(er2, 3));
+else;
+	disp ('there are two non-real roots')
+	disp(round(er1, 3));
+	disp(round(er2, 3));
+end
 
 % End your code
 disp('Completed');
