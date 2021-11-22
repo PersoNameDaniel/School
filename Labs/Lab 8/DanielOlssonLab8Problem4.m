742ab8f44951ef837937b056962708fc35baaaa5
PersoNameDaniel <dmolsson@outlook.com>
2021-11-22 00:24:19 -0600

    Problem 4 Done

--- Labs/Lab 8/DanielOlssonLab8Problem4.m
+++ Labs/Lab 8/DanielOlssonLab8Problem4.m
@@ -18,6 +18,43 @@ disp('Starting L8 P4');
 
 % assigned questions
 
+grades = [];
+A = 0;
+B = 0;
+C = 0;
+D = 0;
+F = 0;
+
+for in = 1:999;
+    uinput = input('Enter numerical grade from 0-100. enter 999 to stop: ');
+    if (uinput == 999);
+        break;
+    else;
+        grades(in) = uinput;
+    end;
+    if (uinput <= 100) & (uinput >= 90);
+        A = A + 1;
+    elseif (uinput < 90) & (uinput >= 80);
+        B = B + 1;
+    elseif (uinput < 80) & (uinput >= 70);
+        C = C + 1;
+    elseif (uinput < 70) & (uinput >= 60);
+        D = D + 1;
+    elseif (uinput < 60) & (uinput >= 0);
+        F = F + 1;
+    else;
+        disp('USER INPUT INVALID PLEASE RESTART');
+        error('INVALID INPUT');
+    end;
+end;
+
+fprintf(' A''s: %d \n B''s: %d \n C''s: %d \n D''s: %d \n F''s: %d \n ', A, B, C, D, F)
+disp('minimum Grade: ')
+disp(min(grades))
+disp('Maximum Grade: ')
+disp(max(grades))
+disp('Average Grade: ')
+disp(mean(grades))
 
 
 % End your code
