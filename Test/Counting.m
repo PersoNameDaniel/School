368ecee5794b9757cf2ea059736d88b126274665
PersoNameDaniel <dmolsson@outlook.com>
2021-11-22 10:53:43 -0600

    1trillion counting file

--- Test/Counting.m
+++ Test/Counting.m
@@ -1,4 +1,25 @@
-while true
-	a = a + 1
-	disp(a)
-end
+clear;
+clc;
+a = 0;
+while a < 1000000000000;
+	a = a + 1;
+%	disp(a)
+	if a == 1000000;
+		disp('1 million');
+	elseif a == 10000000;
+		disp('10 million');
+	elseif a == 100000000;
+		disp('100 million');
+	elseif a == 1000000000;
+		disp('1 billion');
+	elseif a == 10000000000;
+		disp('10 billion');
+	elseif a == 100000000000;
+		disp('100 billion');
+	elseif a == 1000000000000;
+		disp('1 Trillion')
+	end;
+end;
+as = string(a);
+disp(as);
+disp('done');
