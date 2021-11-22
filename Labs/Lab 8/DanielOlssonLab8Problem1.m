2971b3adb6b625f6f9c1ff316838cc133e0c4697
PersoNameDaniel <dmolsson@outlook.com>
2021-11-21 23:11:36 -0600

    Finishing Problem 1

--- Labs/Lab 8/DanielOlssonLab8Problem1.m
+++ Labs/Lab 8/DanielOlssonLab8Problem1.m
@@ -19,17 +19,35 @@ disp('Starting L8 P1');
 % assigned questions
 
 % asigning Values
-R = [0:0.1:40]
-P = R .* ((10) ./ (R + 20)).^2
+R = [0:0.1:40];
+P = R .* ((10) ./ (R + 20)).^2;
 
 % Plotting
-plot(P, R)
+plot(P, R);
 
-grid on
+grid on;
 
-title('Power Versus Resistance')
-xlabel('Power')
-ylabel('Resistance')
+title('Power Versus Resistance');
+xlabel('Power');
+ylabel('Resistance');
+
+% Calculating Values
+
+POld = -1;
+
+for Rl = [0:0.001:100000];
+
+    P = Rl .* ((10) ./ (Rl + 20)).^2;
+
+    if POld > P;
+        POlds = string(POld);
+        Rls = string(Rl);
+        fprintf('%s is the resistance value and %s is the power value \n', Rls, POlds);
+        break;
+    end;
+    POld = P;
+end;
+
 
 % End your code
 disp('Completed');
