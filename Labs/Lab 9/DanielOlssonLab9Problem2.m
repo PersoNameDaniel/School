57d1dd012cad6bdbda45034acb69e4ca1324460e
PersoNameDaniel <dmolsson@outlook.com>
2021-12-06 14:37:29 -0600

    Problem 2 Done

--- Labs/Lab 9/DanielOlssonLab9Problem2.m
+++ Labs/Lab 9/DanielOlssonLab9Problem2.m
@@ -17,8 +17,15 @@ disp('Starting L9 P2');
 % Start your source code here
 
 % assigned questions
-
-
+% 1)
+x = [-10:0.2:10];
+plot(x, f(x));
+title('Plot [x, f(x)]')
 
 % End your code
 disp('Completed');
+
+% Functions
+function [y] = f(x);
+    y = 0.8.*(x.^2).*(exp(-0.5.*x))+(2.5.*(x.^3).*exp(-0.5.*x));
+end
