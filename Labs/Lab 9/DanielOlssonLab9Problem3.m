62ed46370ccf638d968cfef95c1d7e4f1acde503
PersoNameDaniel <dmolsson@outlook.com>
2021-12-06 15:00:31 -0600

    Problem 3 Done

--- Labs/Lab 9/DanielOlssonLab9Problem3.m
+++ Labs/Lab 9/DanielOlssonLab9Problem3.m
@@ -18,7 +18,22 @@ disp('Starting L9 P3');
 
 % assigned questions
 
+Fq = [10:1:10000];
+R = input('Please enter resistance value in ohms: ');
+C = input('Please enter capacitance value in MICRO farads: ') / 1000000;
+
+%hold on;
+plot(Fq, RCFilt(R, C, Fq));
+title('Filter Frequencies');
+xlabel('Frequency');
+ylabel('RV value');
 
 
 % End your code
 disp('Completed');
+
+% Functions
+function [RV] = RCFilt(R, C, Fq);
+    w = 2*pi*Fq;
+    RV = (w*R*C)./sqrt(1+(w.^2).*(R.^2).*(C.^2));
+end
