a4bcac39a48e588180b742ac2eade2ddf09cc457
PersoNameDaniel <dmolsson@outlook.com>
2021-12-06 13:15:22 -0600

    Lab 9 Setup

---
+++ Labs/Lab 9/DanielOlssonLab9Problem3.m
@@ -0,0 +1,24 @@
+%{
+Class : ENGR15100: Software Tools for Engineers
+Instructor : Xiaoli Yang
+Author : [Daniel Olsson]
+Assignment : Lab [9]
+File Name : DanielOlssonLab9Problem3.m
+(eg: Xiaoli_Yang_LAB1_Problem1.m)
+Date : [12]/[4]/[2021]
+%}
+% clear workspace
+clear
+% clear screen
+clc
+% display your name
+disp('Daniel Olsson');
+disp('Starting L9 P3');
+% Start your source code here
+
+% assigned questions
+
+
+
+% End your code
+disp('Completed');
